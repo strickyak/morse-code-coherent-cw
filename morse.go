f7ead50a92a63b32976c351737f7d5d3dece9dc8
strick <strick@yak.net>
2016-11-03 13:24:35 -0700

    gofmt

--- morse.go
+++ morse.go
@@ -23,99 +23,103 @@ http://unix.stackexchange.com/questions/120701/what-is-the-modern-equivalent-of-
 package main
 
 import (
-  "os"
-  "strings"
+	"os"
+	"strings"
 )
 
-const speed = 1.0  // Use 1.0 for normal 12wpm CCW.
+const speed = 1.0 // Use 1.0 for normal 12wpm CCW.
 
-var Code = map[rune]string {
-  'a': ".-",
-  'b': "-...",
-  'c': "-.-.",
-  'd': "-..",
-  'e': ".",
-  'f': "..-.",
-  'g': "--.",
-  'h': "....",
-  'i': "..",
-  'j': ".---",
-  'k': "-.-",
-  'l': ".-..",
-  'm': "--",
-  'n': "-.",
-  'o': "---",
-  'p': ".--.",
-  'q': "--.-",
-  'r': ".-.",
-  's': "...",
-  't': "-",
-  'u': "..-",
-  'v': "...-",
-  'w': ".--",
-  'x': "-..-",
-  'y': "-.--",
-  'z': "--..",
-  '1': ".----",
-  '2': "..---",
-  '3': "...--",
-  '4': "....-",
-  '5': ".....",
-  '6': "-....",
-  '7': "--...",
-  '8': "---..",
-  '9': "----.",
-  '0': "-----",
-  '/': "-..-.",
-  '.': ".-.-.-",
-  ',': "--..--",
+var Code = map[rune]string{
+	'a': ".-",
+	'b': "-...",
+	'c': "-.-.",
+	'd': "-..",
+	'e': ".",
+	'f': "..-.",
+	'g': "--.",
+	'h': "....",
+	'i': "..",
+	'j': ".---",
+	'k': "-.-",
+	'l': ".-..",
+	'm': "--",
+	'n': "-.",
+	'o': "---",
+	'p': ".--.",
+	'q': "--.-",
+	'r': ".-.",
+	's': "...",
+	't': "-",
+	'u': "..-",
+	'v': "...-",
+	'w': ".--",
+	'x': "-..-",
+	'y': "-.--",
+	'z': "--..",
+	'1': ".----",
+	'2': "..---",
+	'3': "...--",
+	'4': "....-",
+	'5': ".....",
+	'6': "-....",
+	'7': "--...",
+	'8': "---..",
+	'9': "----.",
+	'0': "-----",
+	'/': "-..-.",
+	'.': ".-.-.-",
+	',': "--..--",
 }
 
 const Len800Hz = 8000 / 800
-var Wave800Hz = [Len800Hz]byte {
-  0, 60, 100, 100, 60, 0, -60+256, -100+256, -100+256, -60+256,
+
+var Wave800Hz = [Len800Hz]byte{
+	0, 60, 100, 100, 60, 0, -60 + 256, -100 + 256, -100 + 256, -60 + 256,
 }
 
 func Millis(tone bool, ms int) {
-  numSamples := int(float64(ms * 8) / speed)
-  buf := make([]byte, numSamples)
-  if tone {
-    for i := 0; i < numSamples; i++ {
-      buf[i] = Wave800Hz[i%Len800Hz]
-    }
-  }
-  os.Stdout.Write(buf)
+	numSamples := int(float64(ms*8) / speed)
+	buf := make([]byte, numSamples)
+	if tone {
+		for i := 0; i < numSamples; i++ {
+			buf[i] = Wave800Hz[i%Len800Hz]
+		}
+	}
+	os.Stdout.Write(buf)
 }
 
 func Dit() {
-  Millis(true, 100)
-  Millis(false, 100)
+	Millis(true, 100)
+	Millis(false, 100)
 }
 func Dah() {
-  Millis(true, 300)
-  Millis(false, 100)
+	Millis(true, 300)
+	Millis(false, 100)
 }
 
 func Vocalize(s string) {
-  for _, r := range strings.ToLower(s) {
-    x, ok := Code[r]
-    if !ok {
-      x = Code['/']  // Use '/' for unknown chars.
-    }
-    for _, c := range x {
-      switch c {
-        case '.': Dit()
-        case '-': Dah()
-        default: panic(x)
-      }
-    }
-    Millis(false, 200)
-  }
-  Millis(false, 400)
+	for _, r := range strings.ToLower(s) {
+		x, ok := Code[r]
+		if !ok {
+			x = Code['/'] // Use '/' for unknown chars.
+		}
+		for _, c := range x {
+			switch c {
+			case '.':
+				Dit()
+			case '-':
+				Dah()
+			default:
+				panic(x)
+			}
+		}
+		Millis(false, 200)
+	}
+	Millis(false, 400)
 }
 
 func main() {
-  for _, w := range os.Args[1:] {
-    Vocalize(w)
-  }
+	for _, w := range os.Args[1:] {
+		Vocalize(w)
+	}
 }
